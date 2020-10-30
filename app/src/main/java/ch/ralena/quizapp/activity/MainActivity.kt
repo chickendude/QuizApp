1e25b67c23906e4dbf102620ba00ff9c0d02d197
chickendude <pokemaster103@gmail.com>
2020-10-30 18:31:12 -0400

    Convert MainActivity.java to kotlin and un/register listener on start and stop.

---
+++ app/src/main/java/ch/ralena/quizapp/activity/MainActivity.kt
@@ -0,0 +1,56 @@
+package ch.ralena.quizapp.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import ch.ralena.quizapp.objects.Quiz
+import ch.ralena.quizapp.view.ViewMvcFactory
+import javax.inject.Inject
+
+/*
+   Icon courtesy of Android Asset Studio:
+   https://romannurik.github.io/AndroidAssetStudio/
+
+   Sound effects in the Public Domain, taken from:
+   http://www.freesound.org/
+*/
+class MainActivity : BaseActivity(), MainActivityView.Listener {
+
+	@Inject	lateinit var viewMvcFactory: ViewMvcFactory
+	private lateinit var viewMvc: MainActivityView
+	private lateinit var quiz: Quiz
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		injector.inject(this)
+		super.onCreate(savedInstanceState)
+		viewMvc = viewMvcFactory.newMainActivityView()
+		setContentView(viewMvc.rootView)
+
+		// create our mQuiz and the answer mButtons
+		quiz = Quiz()
+
+		// create a random question at the start
+		var question = quiz.question
+		question += " ...?"
+		viewMvc.loadQuestionText(question)
+	}
+
+	override fun onStart() {
+		super.onStart()
+		viewMvc.registerListener(this)
+	}
+
+	override fun onStop() {
+		super.onStop()
+		viewMvc.unregisterListener(this)
+	}
+
+	override fun onStartButtonClicked() {
+		val intent = Intent(this, QuizActivity::class.java)
+		startActivity(intent)
+	}
+
+	companion object {
+		private val TAG = View::class.java.simpleName
+	}
+}
